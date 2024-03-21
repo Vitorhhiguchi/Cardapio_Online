e4f47e07caf9fa5f425caaf36676395ae43afe8f
Vitor Higuchi <130410982+Vitorhhiguchi@users.noreply.github.com>
2024-03-21 20:42:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -180,7 +180,7 @@ checkoutBtn.addEventListener("click", function(){
 function checkRestaurantOpen(){
     const data = new Date();
     const hora = data.getHours();
-    return hora >= 18 && hora < 22;  
+    return hora >= 18 && hora < 20;  
     // True = restaurante esta aberto
 }
 
